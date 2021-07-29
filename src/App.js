815731988b1873a675442a5c5e0c2e86566ba06b
Lovish-Aggarwal <lovishagg@gmail.com>
2021-07-29 19:34:59 +0530

    Addition of sass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Board  from "./components/Board";
 
+import "./styles/root.scss";
+
 const App = ()=>{
   return (
-    <div>
+    <div className="app">
       <h1>Tic-Tac-Toe</h1>
       <Board />
     </div>
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
 const Square = ({value}) => {
-    return (
-        <button type="button">{value}</button>
-    )
+    return <button type="button" className="square">{value}</button>
 }
 
 export default Square
