6eb7e553d8c7b3bce9fa4577b691dffd7508c388
Lovish-Aggarwal <lovishagg@gmail.com>
2021-07-29 15:44:39 +0530

    Created board components and sqaures of the board

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
+import Board  from "./components/Board";
 
 const App = ()=>{
   return (
-    <>
-    <h1>Welcome to React Parcel Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-    <b>I am changing this file to see changes in the app</b>
-  </>
+    <div>
+      <h1>Tic-Tac-Toe</h1>
+      <Board />
+    </div>
   )
 }
 
