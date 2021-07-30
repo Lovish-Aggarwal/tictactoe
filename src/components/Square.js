099e9f4004cfce072a48096ece6ce5a256eb2224
Lovish-Aggarwal <lovishagg@gmail.com>
2021-07-30 14:43:14 +0530

    Handle states using usestate

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
-const Square = ({value}) => {
-    return <button type="button" className="square">{value}</button>
+const Square = ({value, onClick}) => {
+    return (
+    <button type="button" className="square" onClick={onClick}>
+        {value}</button>
+    )
 }
 
 export default Square
